576e8f2ead6a5e3b1876032c3a23d2d625610f08
joey.hi <joey.hi@kakaocorp.com>
2023-09-21 15:40:20 +0900

    [feat] vectorDB에 정보 업로드

--- chatbot/chatbot/chatbot.py
+++ chatbot/chatbot/chatbot.py
@@ -5,36 +5,94 @@ from datetime import datetime
 # Import pynecone.
 import pynecone as pc
 import os
+import chromadb
 from pynecone.base import Base
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import (
+    TextLoader
+)
 
 # openai.api_key = "<YOUR_OPENAI_API_KEY>"
 f = open("./chatbot/apiKey.txt", 'r')
 line = f.readline()
 f.close()
 os.environ["OPENAI_API_KEY"] = line
-
-file = open('./kakao_sync_data.txt', 'r')
-kakao_sync_data = file.read()
-file.close()
-
 llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo-16k')
 
+LOADER_DICT = {
+    "txt": TextLoader,
+}
+DATA_DIR = os.path.dirname(os.path.relpath('./data/'))
+CHROMA_PERSIST_DIR = os.path.join(DATA_DIR, "upload/chroma-persist")
+CHROMA_COLLECTION_NAME = "kakao-bot"
 
-def make_prompt_template() -> PromptTemplate:
-    return PromptTemplate(
-        input_variables=["text"],
-        template="사용자의 질문에 대한 적절한 답변을 주어진 정보에서 찾아서 반환해야 한다. \n답변은 두 문단 내로 간략하게 제공한다. \n<정보>: \n"
-                 + kakao_sync_data + "\n <질문>: {text}"
+
+def upload_embedding_from_file(file_path):
+    loader = LOADER_DICT.get(file_path.split(".")[-1])
+    if loader is None:
+        raise ValueError("Not supported file type")
+    documents = loader(file_path).load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100) # 잘라서 주고, 그 사이에 overlap 줘서 자른다 (의미 손실을 막기 위해서).
+    docs = text_splitter.split_documents(documents)
+
+    Chroma.from_documents( # ChromaDB에 저장함.
+        docs,
+        OpenAIEmbeddings(), # openai의 임베딩 사용
+        collection_name=CHROMA_COLLECTION_NAME,
+        persist_directory=CHROMA_PERSIST_DIR,
+    )
+    print('db success')
+
+
+def upload_embeddings_from_dir(dir_path):
+    failed_upload_files = []
+
+    for root, dirs, files in os.walk(dir_path):
+        print(files)
+        for file in files:
+            if file.endswith(".txt"):
+                file_path = os.path.join(root, file)
+
+                try:
+                    upload_embedding_from_file(file_path)
+                    print("SUCCESS: ", file_path)
+                except Exception as e:
+                    print("FAILED: ", file_path + f"by({e})")
+                    failed_upload_files.append(file_path)
+
+
+def ask_to_chatbot(text):
+    upload_embeddings_from_dir(os.path.relpath('./data/'))
+    db = Chroma(
+        persist_directory=CHROMA_PERSIST_DIR,
+        embedding_function=OpenAIEmbeddings(),
+        collection_name=CHROMA_COLLECTION_NAME,
     )
 
+    docs = db.similarity_search(text)
+    print(docs)
 
-def ask_about_kakao_sync(text) -> str:
-    prompt_template = make_prompt_template()
-    chain = LLMChain(llm=llm, verbose=True, prompt=prompt_template)
-    return chain.run(text)
+    return docs
+
+#
+# def make_prompt_template() -> PromptTemplate:
+#     return PromptTemplate(
+#         input_variables=["text"],
+#         template="사용자의 질문에 대한 적절한 답변을 주어진 정보에서 찾아서 반환해야 한다. \n답변은 두 문단 내로 간략하게 제공한다. \n<정보>: \n"
+#                  + kakao_sync_data + "\n <질문>: {text}"
+#     )
+
+#
+# def ask_about_kakao_sync(text) -> str:
+#     prompt_template = make_prompt_template()
+#     chain = LLMChain(llm=llm, verbose=True, prompt=prompt_template)
+#     return chain.run(text)
 
 
 class Message(Base):
@@ -53,7 +111,7 @@ class State(pc.State):
     def output(self):
         if not self.text.strip():
             return "Answer will appear here."
-        self.answer = ask_about_kakao_sync(self.text)
+        self.answer = ask_to_chatbot(self.text)
 
     def post(self):
         self.output()
